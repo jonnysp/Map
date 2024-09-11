56105b89c546ac85bbe3f3bede2235b21cb7bbcb
zonky2 <info@e-spin.de>
2024-09-11 19:49:37 +0200

    Fix PHP 8 warning

--- src/Resources/contao/dca/tl_map_points.php
+++ src/Resources/contao/dca/tl_map_points.php
@@ -73,7 +73,7 @@ $GLOBALS['TL_DCA']['tl_map_points'] = array
 				'label'               => &$GLOBALS['TL_LANG']['tl_map_points']['delete'],
 				'href'                => 'act=delete',
 				'icon'                => 'delete.svg',
-				'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+				'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? '') . '\'))return false;Backend.getScrollOffset()"'
 			),
 			'show' => array
 			(
