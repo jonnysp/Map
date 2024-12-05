622752e69b05ccaa2cdf5394509d0ab2d6fcb629
Werner Joss <werner@hoernerfranzracing.de>
2024-12-05 17:07:49 +0100

    better formatting

--- src/Resources/contao/languages/en/tl_map_points.php
+++ src/Resources/contao/languages/en/tl_map_points.php
@@ -11,6 +11,8 @@ $GLOBALS['TL_LANG']['tl_map_points']['position'] = array('Position', 'Map-Point 
 $GLOBALS['TL_LANG']['tl_map_points']['info'] 	 = array('Infobubble','An / Aus');
 $GLOBALS['TL_LANG']['tl_map_points']['toggle'] 	 = array('AN/AUS','An / Aus');
 
+
+
 /**
  * Legends
  */
@@ -19,3 +21,12 @@ $GLOBALS['TL_LANG']['tl_map_points']['title_legends'] = 'Map-Points';
 $GLOBALS['TL_LANG']['tl_map_points']['image_legend'] = 'Image';
 $GLOBALS['TL_LANG']['tl_map_points']['description_legend'] = 'Beschreibungen';
 
+/**
+ * Buttons
+ */
+$GLOBALS['TL_LANG']['tl_map_points']['new']        = array('Neuer Map-Point', 'Einen neuen Map-Point anlegen');
+$GLOBALS['TL_LANG']['tl_map_points']['show']       = array('Map-Point details', 'Details des Map-Point ID %s');
+$GLOBALS['TL_LANG']['tl_map_points']['edit']       = array('Map-Point bearbeiten', 'Map-Point ID %s bearbeiten');
+$GLOBALS['TL_LANG']['tl_map_points']['copy']       = array('Map-Point kopieren', 'Map-Point ID %s kopieren');
+$GLOBALS['TL_LANG']['tl_map_points']['delete']     = array('Map-Point löschen','Map-Point ID %s löschen');
+$GLOBALS['TL_LANG']['tl_map_points']['cut']    	= array('Map-Point Verschieben', 'Verschiebe Map-Point ID %s');
