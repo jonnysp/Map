8aa7ef21513d979104c72402de7d27db7393c594
jonnysp <jonnyspitzner@googlemail.com>
2024-12-06 10:38:55 +0100

    Fix Edit Popup

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -27,31 +27,24 @@ class tl_content_map extends Backend
 
 	public function getMap()
 	{
-		$objCats =  MapModel::findAll();
-		$arrCats = array();
-		foreach ($objCats as $objCat)
+		$objMaps =  MapModel::findAll();
+		$arrMaps = array();
+		foreach ($objMaps as $objMap)
 		{
-			$arrCats[$objCat->id] = '[ID ' . $objCat->id . '] - '. $objCat->title;
+			$arrMaps[$objMap->id] = '[ID ' . $objMap->id . '] - '. $objMap->title;
 		}
-		return $arrCats;
+		return $arrMaps;
 	}
 
 	public function editMap(DataContainer $dc)
 	{
 
-
-//		$this->loadLanguageFile('tl_map');
-//
-//		$title = "Edit"; //sprintf($GLOBALS['TL_LANG']['tl_map']['editheader'], $dc->value)"";
-//
-//		$href = System::getContainer()->get('router')->generate('contao_backend', array('do'=>'map', 'table'=>'tl_map', 'id'=>$dc->value, 'popup'=>'1', 'nb'=>'1'));
-//
-//		return ' <a href="' . StringUtil::specialcharsUrl($href) . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.openModalIframe({\'title\':\'' . StringUtil::specialchars(str_replace("'", "\\'", $title)) . '\',\'url\':this.href});return false">' . Image::getHtml('alias.svg', $title) . '</a>';
-		
-		$requestToken = System::getContainer()->get('contao.csrf.token_manager')->getDefaultTokenValue();
 		$this->loadLanguageFile('tl_map');
-		return ($dc->value < 1) ? '' : ' <a href="contao/main.php?do=map&amp;act=edit&amp;id=' . $dc->value . '&amp;popup=1&amp;nb=1&amp;rt=' . $requestToken . '" title="' . sprintf(StringUtil::specialchars($GLOBALS['TL_LANG']['tl_map']['editheader'][1]), $dc->value) . '" onclick="Backend.openModalIframe({\'title\':\'' . StringUtil::specialchars(str_replace("'", "\\'", sprintf($GLOBALS['TL_LANG']['tl_map']['editheader'][1], $dc->value))) . '\',\'url\':this.href});return false">' . Image::getHtml('alias.svg', $GLOBALS['TL_LANG']['tl_map']['editheader'][0]) . '</a>';
 
+		$title = sprintf($GLOBALS['TL_LANG']['tl_map']['editheader'][1], $dc->value);
+		$href = System::getContainer()->get('router')->generate('contao_backend', array('do'=>'map', 'table'=>'tl_map','act'=>'edit', 'id'=>$dc->value , 'popup'=>'1', 'nb'=>'1'));
+		return ' <a href="' . StringUtil::specialcharsUrl($href) . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.openModalIframe({\'title\':\'' . StringUtil::specialchars(str_replace("'", "\\'", $title)) . '\',\'url\':this.href});return false">' . Image::getHtml('alias.svg', $title) . '</a>';
+		
 	}
 
 }
