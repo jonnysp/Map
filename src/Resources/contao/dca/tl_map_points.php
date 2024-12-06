a5fc171b253213fb07b32e2559577327468c3494
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-12-06 15:55:46 +0100

    Use `'%contao.image.valid_extensions%'` to get the valid extensions

--- src/Resources/contao/dca/tl_map_points.php
+++ src/Resources/contao/dca/tl_map_points.php
@@ -82,8 +82,7 @@ $GLOBALS['TL_DCA']['tl_map_points'] = array
 		(
 			'label'                   => &$GLOBALS['TL_LANG']['tl_map_points']['image'],
 			'inputType'               => 'fileTree',
-			// 'eval'                    => array('fieldType'=>'radio', 'files'=>true, 'filesOnly'=>true, 'extensions'=>$GLOBALS['TL_CONFIG']['validImageTypes']),
-		    'eval'                    => array('fieldType'=>'radio', 'files'=>true, 'filesOnly'=>true, 'extensions'=>['jpg','jpeg','png']),
+			'eval'                    => array('fieldType'=>'radio', 'files'=>true, 'filesOnly'=>true, 'extensions'=>'%contao.image.valid_extensions%'),
 			'sql'                     => ['type' => 'binary','notnull' => false,'length' => 16,'fixed' => true]
 		),
 		'description' => array
