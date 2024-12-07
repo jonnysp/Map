ee2abd3e97976de06e207983030ef2c2a360be9f
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2024-12-07 12:42:31 +0100

    Fix null warning on content element selection
    * also use proper return types

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,27 +18,30 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['map'] = array
 	'sql'                     => "int(10) unsigned NOT NULL default '0'"
 );
 
-class tl_content_map extends Backend 
+class tl_content_map extends Backend
 {
-
-	public function getMap()
+	public function getMap(): array
 	{
-		$objMaps = MapModel::findAll();
 		$arrMaps = array();
-		foreach ($objMaps as $objMap)
+
+		if ($objMaps = MapModel::findAll())
 		{
-			$arrMaps[$objMap->id] = '[ID ' . $objMap->id . '] - '. $objMap->title;
+			foreach ($objMaps as $objMap)
+			{
+				$arrMaps[$objMap->id] = '[ID ' . $objMap->id . '] - '. $objMap->title;
+			}
 		}
+
 		return $arrMaps;
 	}
 
-	public function editMap(DataContainer $dc)
+	public function editMap(DataContainer $dc): string
 	{
 		$this->loadLanguageFile('tl_map');
 
 		$title = sprintf($GLOBALS['TL_LANG']['tl_map']['editheader'][1], $dc->value);
 		$href = System::getContainer()->get('router')->generate('contao_backend', array('do'=>'map', 'table'=>'tl_map','act'=>'edit', 'id'=>$dc->value , 'popup'=>'1', 'nb'=>'1'));
+
 		return ' <a href="' . StringUtil::specialcharsUrl($href) . '" title="' . StringUtil::specialchars($title) . '" onclick="Backend.openModalIframe({\'title\':\'' . StringUtil::specialchars(str_replace("'", "\\'", $title)) . '\',\'url\':this.href});return false">' . Image::getHtml('alias.svg', $title) . '</a>';
 	}
-
 }
